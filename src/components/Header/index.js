9dce8674bcf88d7cdada41e254bb5aaeeb91c94c
Jonathan Palacio <jpalacio0612@gmail.com>
2020-10-15 19:38:11 -0500

    header from material-ui added

---
+++ src/components/Header/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import { useStyles } from './styles';
+
+export const Header = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Agent Match
+          </Typography>
+          <Button color="inherit">Login</Button>
+          <Button color="inherit">Register</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
